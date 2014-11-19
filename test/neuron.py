8eb2de84ac72705a350289459f1fb0aaf9d5a388
FXHibon <djxf44@gmail.com>
2014-11-19 15:46:41 +0100

    Adding Neuron test

--- test/neuron.py
+++ test/neuron.py
@@ -4,13 +4,19 @@ from math import exp
 
 
 class Neurone:
-    def __init__(self, weight, seuil, pas):
+    # Construit le neurone
+    # weight: liste des poids
+    # threshold: seuil
+    # pas: utilisé dans le calcul des nouveaux poids
+    #
+    def __init__(self, weight, threshold, pas):
         self.input = []
         self.weight = weight
         self.state = False
-        self.seuil = seuil
+        self.threshold = threshold
         self.pas = pas
 
+    # Sum pondéré des (input, poids)
     def sum(self):
         res = 0
         i = 0
@@ -20,15 +26,19 @@ class Neurone:
 
         return res
 
+    # Fonciton d'activation: sigmoide
     def activation(self, x):
         return 1 / (1 + exp(-x))
 
+    # calcule l'état du neurone, activé ou non
     def evaluate(self):
-        if self.activation(self.sum()) >= self.seuil:
+        if self.activation(self.sum()) >= self.threshold:
             self.state = True
         else:
             self.state = False
 
+    # prend les valeurs d'entrée 'input', les valeurs attendues 'output' et entraîne le neurone
+    # (recalcul des poids à chaque itération)
     def train(self, input, output):
         self.input = input
         self.evaluate()
@@ -42,11 +52,13 @@ class Neurone:
 
 if __name__ == "__main__":
     n = Neurone([2, 2], 0.8, 1)
+
+    # entraine le neurone pour la fonction OR
     for i in range(100):
         n.train([1, 1], 1)
         n.train([1, 0], 1)
         n.train([0, 1], 1)
-        n.train([0, 0], 0)
+        # n.train([0, 0], 0)
 
     n.input = [0, 1]
     n.evaluate()
