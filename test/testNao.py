0fcd3a69baf68a9972ea5a1b7d2676b8b4faa49d
FXHibon <djxf44@gmail.com>
2015-05-07 08:20:14 +0200

    nao trqcker

--- test/testNao.py
+++ test/testNao.py
@@ -2,50 +2,55 @@ from naoqi import ALProxy
 
 __author__ = 'fx'
 
-NAO_ADDRESS = "127.0.0.1"
-NAO_PORT = 52961
+NAO_ADDRESS = "172.16.6.117"
+NAO_PORT = 9559
+"""
+This example shows how to use ALRedBallTracker.
+It is launched for a little while, then stopped.
+"""
 
-tts = ALProxy("ALTextToSpeech", NAO_ADDRESS, NAO_PORT)
-motion = ALProxy("ALMotion", NAO_ADDRESS, NAO_PORT)
-motion.setStiffnesses("Body", 1.0)
-
-
-def say(text):
-    """
-    Make Nao say text
-    :param text: Text to speak
-    """
-    print("say(", text, ")")
-    tts.say(text)
+from naoqi import ALProxy, ALModule, ALBroker
 
 
-def moveTo(x, y, theta, async=False):
-    """
-    Move Nao
-    :param x: Distance along the X axis in meters
-    :param y: Distance along the Y axis in meters
-    :param theta: Rotation around the Z axis in radians [-3.1415 to 3.1415]
-    :param async: background call or not
-    """
-    print("moveTo(", y, ", ", y, ", ", theta, ", ", async, ")")
-    motion.moveInit()
+class BallDetector(ALModule):
+    def __init(self, name):
+        print("__init__")
+        ALModule.__init__(self, name)
 
-    if async:
-        taskId = motion.post.moveTo(x, y, theta)
-        print("taskId = ", taskId)
-        return taskId
-    else:
-        motion.moveTo(x, y, theta)
+    def onBallDetected(self, key, value, message):
+        print("ball detected: " + key + ", " + value + ", " + message)
 
 
-def stopMove():
-    motion.stopMove()
+IP = "172.16.6.117"
+PORT = 9559
 
+print "Connecting to", IP, "with port", PORT
+myBroker = ALBroker("myBroker",
+                    "0.0.0.0",  # listen to anyone
+                    0,  # find a free port and use it
+                    IP,  # parent broker IP
+                    PORT)  # parent broker port
 
-if __name__ == '__main__':
+motion = ALProxy("ALMotion", IP, PORT)
+redBallTracker = ALProxy("ALRedBallTracker", IP, PORT)
+memory = ALProxy("ALMemory", IP, PORT)
 
-    print("Enter stop to exit")
-    input = raw_input("$ ")
-    while input != "stop":
-        input = raw_input("$ ")
-        eval(input)
+ballDetector = BallDetector("BallDetector")
+memory.subscribeToEvent("redBallDetected", "ballDetector", "onBallDetected")
+
+# First, set Head Stiffness to ON.
+motion.setStiffnesses("Head", 1.0)
+
+# Then, start tracker.
+redBallTracker.startTracker()
+
+print "ALRedBallTracker successfully started, now show a red ball face !"
+
+# Wait an input
+input()
+
+# Stop tracker and remove head stiffness.
+redBallTracker.stopTracker()
+motion.setStiffnesses("Head", 0.0)
+
+print "ALRedBallTracker stopped."
